167754bfc1a937e1e5824540ad0139ea32857f5b
Aicrag19 <delenyer19@gmail.com>
2025-11-01 10:04:00 -0400

    commit buena

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import './App.css'
-import Cartas from './components/carta'
+import "./App.css";
+import Cartas from "./components/Carta";
 
-const mostrarCartas =(numero:number ,pinta:string)=>{
-    alert(`Carta de numero:${numero} y de pinta ${pinta}`);
-  }
-
+const mostrarCartas = (numero: number, pinta: string) => {
+	alert(`Carta de numero:${numero} y de pinta ${pinta}`);
+};
 
 function App() {
-  return (
-<div>
-  <h1>Hola mundo</h1>
-<Cartas numero={5} pinta ={"Trebol ♣"} mostrarCartas={mostrarCartas} />
-<Cartas numero={2} pinta ={"corazon ♥"} mostrarCartas={mostrarCartas}/>
-<Cartas numero={3} pinta ={"pica ♠"} mostrarCartas={mostrarCartas}/>
-<Cartas numero={4} pinta ={"diamante 💎"} mostrarCartas={mostrarCartas}/>
-</div>
-    
-  )
-  
+	return (
+		<div>
+			<h1>Hola mundo</h1>
+			<Cartas numero={5} pinta={"Trebol ♣"} mostrarCartas={mostrarCartas} />
+			<Cartas numero={2} pinta={"corazon ♥"} mostrarCartas={mostrarCartas} />
+			<Cartas numero={3} pinta={"pica ♠"} mostrarCartas={mostrarCartas} />
+			<Cartas numero={4} pinta={"diamante 💎"} mostrarCartas={mostrarCartas} />
+		</div>
+	);
 }
 
-export default App
+export default App;
