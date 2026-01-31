d929e299b0d1707e9e4570a60bcf7311c47acdc2
Aicrag19 <delenyer19@gmail.com>
2026-01-31 08:04:26 -0600

    Update Carta.tsx

--- src/components/Carta.tsx
+++ src/components/Carta.tsx
@@ -1,16 +1,29 @@
 type Props = {
-	numero: number;
-	pinta: string;
-	mostrarCartas: (numero: number, pinta: string) => void;
+	nombre: string;
+	mensaje: string;
+	imagen: string;
+	hora: number;
 };
-function Cartas({ numero, pinta, mostrarCartas }: Props) {
-	const handleClick = () => {
-		mostrarCartas(numero, pinta);
-	};
+
+function Cartas({ nombre, mensaje, imagen, hora }: Props) {
 	return (
-		<button onClick={handleClick}>
-			<h1>Soy una Carta{numero + 300 + " " + pinta}</h1>
-		</button>
+		<div className="flex border-4 bg-white w-2/4">
+			<img
+				className="border-4 bg-white-500 size-14"
+				src={imagen}
+				alt={nombre}
+			></img>
+
+			<div className="flex justify-between border-4 w-full">
+				<div className="flex flex-col">
+					<h1>{nombre}</h1>
+					<h2>{mensaje}</h2>
+				</div>
+				<div className="">
+					<h1>hora{hora}</h1>
+				</div>
+			</div>
+		</div>
 	);
 }
 export default Cartas;
